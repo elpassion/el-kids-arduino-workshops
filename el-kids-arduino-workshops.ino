183c8293477a61a5cbb5377ddde0319674423085
Mariusz Kulerski <mariusz.kulerski@elpassion.pl>
2017-06-02 10:44:02 +0200

    step 1: introduction

---
+++ el-kids-arduino-workshops.ino
@@ -0,0 +1,13 @@
+#include "setup.h"
+
+// Wstęp
+// 1. Jak robot wykonuje swój kod (pętla powtarzaj)
+// 2. Spróbujmy robotowi dojechać do mety, funkcje które mogą się przydać:
+//      - jedz_do_przodu(milisekundy)
+//      - zatrzymaj_sie()
+//      - poczekja(milisekundy)
+
+procedura powtarzaj() {
+  //tu wpisz instrukcje dla robota
+}
+
---
+++ setup.h
@@ -0,0 +1,54 @@
+#include <L298N.h>
+#include <NewPing.h>
+
+// Robot setup
+
+#define LEFT_FORWARD 2
+#define LEFT_BACKWARD 4
+#define LEFT_SPEED 3
+#define RIGHT_FORWARD 6
+#define RIGHT_BACKWARD 7
+#define RIGHT_SPEED 5
+
+#define PROXIMITY_ECHO 10
+#define PROXIMITY_TRIG 9
+
+#define CENTER_LINE_DETECTOR 12
+
+#define DEFAULT_SPEED 200
+
+// Language translations
+
+#define nic void
+#define powtarzaj loop
+#define jezeli if
+#define w_przeciwnym_razie else 
+#define procedura void
+#define poczekaj delay
+
+// Functions and the backstage code
+
+NewPing radar(PROXIMITY_TRIG, PROXIMITY_ECHO, 200);
+L298N wheels(LEFT_SPEED, LEFT_FORWARD, LEFT_BACKWARD, RIGHT_FORWARD, RIGHT_BACKWARD, RIGHT_SPEED);
+
+void setup() { }
+
+// Available wheels methods:
+//    void forward(int speed, int delay_time);
+//    void full_stop(int delay_time);
+//    void turn_right(int speed, int delay_time);
+//    void turn_left(int speed, int delay_time);
+//    void backward(int speed, int delay_time);
+
+boolean na_linii(){
+  return digitalRead(CENTER_LINE_DETECTOR) == HIGH;
+}
+
+void zatrzymaj_sie(){
+  wheels.full_stop(0);
+}
+
+void jedz_do_przodu(int time){
+  wheels.forward(DEFAULT_SPEED, time);
+}
+
