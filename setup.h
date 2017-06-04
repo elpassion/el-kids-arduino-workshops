f553bc0d6eb3349e296b13b70aa8adfa032bb367
Karol Sarnacki <karol.sarnacki@elpassion.com>
2017-06-04 04:28:59 +0200

    Improve some wording

--- setup.h
+++ setup.h
@@ -7,15 +7,15 @@
 
 // Robot setup
 
-#define LEFT_FORWARD 2
-#define LEFT_BACKWARD 4
-#define LEFT_SPEED 3
-#define RIGHT_FORWARD 6
+#define LEFT_FORWARD   2
+#define LEFT_BACKWARD  4
+#define LEFT_SPEED     3
+#define RIGHT_FORWARD  6
 #define RIGHT_BACKWARD 7
-#define RIGHT_SPEED 5
+#define RIGHT_SPEED    5
 
 #define PROXIMITY_ECHO 10
-#define PROXIMITY_TRIG 9
+#define PROXIMITY_TRIG  9
 
 #define CENTER_LINE_DETECTOR 12
 
@@ -26,7 +26,7 @@
 NewPing radar(PROXIMITY_TRIG, PROXIMITY_ECHO, 200);
 L298N wheels(LEFT_SPEED, LEFT_FORWARD, LEFT_BACKWARD, RIGHT_FORWARD, RIGHT_BACKWARD, RIGHT_SPEED);
 
-void setup() { }
+procedura ustaw() { }
 
 // Available wheels methods:
 //    void forward(int speed, int delay_time);
@@ -35,36 +35,37 @@ void setup() { }
 //    void turn_left(int speed, int delay_time);
 //    void backward(int speed, int delay_time);
 
-boolean na_linii(){
-  return digitalRead(CENTER_LINE_DETECTOR) == HIGH;
-}
-
-void zatrzymaj_sie(){
+procedura zatrzymaj_sie() {
   wheels.full_stop(200);
 }
 
-void jedz_do_przodu(int time){
+procedura jedz_do_przodu(int time) {
   wheels.forward(DEFAULT_SPEED, time);
 }
 
-void jedz_do_tylu(int time){
+procedura jedz_do_tylu(int time) {
   wheels.backward(DEFAULT_SPEED, time);
 }
 
-void obrot_w_lewo(int time){
+procedura obroc_w_lewo(int time) {
   wheels.turn_left(DEFAULT_SPEED, time); 
 }
 
-void obrot_w_prawo(int time){
+procedura obroc_w_prawo(int time) {
   wheels.turn_right(DEFAULT_SPEED, time); 
 }
 
-int odlegosc(){
-  int distance = radar.ping_cm();
-  if(distance == 0){
-    return 10000;
-  }else{
-    return distance;
+podaj czy na_linii() {
+  wynik digitalRead(CENTER_LINE_DETECTOR) == HIGH;
+}
+
+podaj liczbe odleglosc() {
+  niech dystans = radar.ping_cm();
+  
+  jezeli (dystans == 0) {
+    wynik 10000;
+  } w_przeciwnym_razie {
+    wynik dystans;
   }
 }
 
--- solutions.h
+++ solutions.h
@@ -1,18 +1,14 @@
 #include "setup.h"
 
-// Poruszanie się
-
-// 1. Jak robot wykonuje swój kod (pętla powtarzaj)
+////////////////////////////////////////////////////////////////////////////////
+// 1. Jak robot wykonuje swój kod.
 
 procedura powtarzaj() {
-  dojedz_do_sciany();
+
 }
 
+////////////////////////////////////////////////////////////////////////////////
 // 2. Spróbujmy pomóc robotowi dojechać do mety.
-//    Funkcje które mogą się przydać:
-//      - jedz_do_przodu(milisekundy)
-//      - zatrzymaj_sie()
-//      - poczekaj(milisekundy)
 
 procedura do_mety() {
   jedz_do_przodu(3000);
@@ -20,32 +16,19 @@ procedura do_mety() {
   poczekaj(5000);
 }
 
-// 3. Czy roboty mogą tańczyć? Spróbujmy zapropogramować robota tak aby to zrobił!
-//    Funkcje które mogą się przydać:
-//      - jedz_do_przodu(milisekundy)
-//      - obrot_w_lewo(milisekundy)
-//      - obrot_w_prawo(milisekundy)
-//      - zatrzymaj_sie()
-//      - poczekaj(milisekundy)
+////////////////////////////////////////////////////////////////////////////////
+// 3. Czy roboty mogą tańczyć? Zapropogramujmy naszego robota tak, by to zrobił!
 
 procedura taniec() {
-  obrot_w_lewo(200);
-  obrot_w_prawo(400);
-  obrot_w_lewo(200);
+  obroc_w_lewo(200);
+  obroc_w_prawo(400);
+  obroc_w_lewo(200);
   jedz_do_przodu(200);
   jedz_do_tylu(200);
 }
 
+////////////////////////////////////////////////////////////////////////////////
 // 4. Czy robot coś widzi? 
-//
-//    /test czujnikow przy pomocy kartonikow/
-//
-//    Spróbujmy zaproprogramować robota tak, aby po dojechaniu do mety -zatrzymał się
-//    Funkcje które mogą się przydać:
-//      - jedz_do_przodu(milisekundy)
-//      - na_linii 
-//        oraz instrukcja 'jezeli' i 'w_przeciwnym_razie'
-//      - zatrzymaj_sie()
 
 procedura do_mety_i_stop() {
   jezeli (na_linii()) {
@@ -56,43 +39,33 @@ procedura do_mety_i_stop() {
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 // 5. Czy robot ma więcej zmysłów? 
-//
-//    /informacja jak dziala czujnik ultradzwiekowy/
-//
-//    Spróbujmy zatrzymać robota przed ścianą
-//    Funkcje które mogą się przydać:
-//      - jedz_do_przodu(milisekundy)
-//      - odleglosc 
-//        oraz instrukcja 'jezeli' i 'w_przeciwnym_razie'
-//        oraz operatory < > 
-//      - zatrzymaj_sie()
 
 procedura dojedz_do_sciany() {
-  jezeli (odlegosc() < 5) {
+  jezeli (odleglosc() < 5) {
     zatrzymaj_sie();
-    poczekaj(500);
+    czekaj(500);
   }
   w_przeciwnym_razie {
     jedz_do_przodu(100);
   }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 // 6. Czy robot może bawić się z innymi robotami?
-//
-// /przedtawienie pomysłu/
 
 procedura berek() {
   jezeli (na_linii()) {
     jedz_do_tylu(500);
-    obrot_w_lewo(500);
+    obroc_w_lewo(500);
   }
   w_przeciwnym_razie {
-    jezeli (odlegosc() < 50) {
+    jezeli (odleglosc() < 50) {
       jedz_do_przodu(100);
     }
     w_przeciwnym_razie {
-      obrot_w_prawo(100);
+      obroc_w_prawo(100);
     }
   }
 }
